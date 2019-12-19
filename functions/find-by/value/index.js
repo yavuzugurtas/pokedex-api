10c01d0a32908fda5ebadd849bcf7d6cff875520
Yavuz Ugurtas <yavuzugurtas@gmail.com>
2019-12-19 16:13:58 +0100

    find-by-value

--- functions/find-by/value/index.js
+++ functions/find-by/value/index.js
@@ -4,6 +4,24 @@
 
 const value = (pokeArray, value) => {
 
+    let emptyArray = [];
+
+    for (let i = 0 ; i<pokeArray.length; i++) {
+  
+      let valuesOfPokearray =Object.values(pokeArray[i]);
+     
+      if (valuesOfPokearray.includes(Number(value)) || valuesOfPokearray.includes(value)=== true) {
+        let emptyObject = {};
+        emptyObject.name = pokeArray[i].name;
+        emptyObject.num = pokeArray[i].num;
+        emptyArray.push(emptyObject);
+        
+       }
+      
+  
+    }
+    return emptyArray;
+
 };
 
 
